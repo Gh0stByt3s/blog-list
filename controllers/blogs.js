f5b71d794d810082f9015b0e91ced86fc2f454e8
Gh0stByt3s <orangageorge.gr@gmail.com>
2023-05-11 12:54:50 +0300

    Refactored promises into async-await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,57 +2,50 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const logger = require("../utils/logger");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => logger.error("Blogs not Found", error.message));
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  if (blogs) {
+    response.json(blogs);
+  } else {
+    response.status(404).end();
+  }
 });
 
-// blogsRouter.get("/:id", (request, response, next) => {
-//   Blog.findById(request.params.id)
-//     .then((blog) => {
-//       if (blog) {
-//         response.json(blog);
-//       } else {
-//         response.status(404).end();
-//       }
-//     })
-//     .catch((error) => logger.error("Blog not Found", error.message));
-// });
-
-blogsRouter.post("/", (request, response, next) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.status(201).json(savedBlog);
-    })
-    .catch((error) => logger.error("Invalid blog post", error.message));
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
 });
 
-// notesRouter.delete("/:id", (request, response, next) => {
-//   Note.findByIdAndRemove(request.params.id)
-//     .then(() => {
-//       response.status(204).end();
-//     })
-//     .catch((error) => logger.error("Blog post not in database", error.message));
-// });
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
 
-// blogsRouter.put("/:id", (request, response, next) => {
-//   const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
 
-//   const blog = {
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
 
-//   };
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
 
-//   Blog.findByIdAndUpdate(request.params.id, note, { new: true })
-//     .then((updatedBlog) => {
-//       response.json(updatedBlog);
-//     })
-//     .catch((error) => logger.error("Blog post not found", error.message));
-// });
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = request.body;
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+
+  response.json(updatedBlog);
+});
 
 module.exports = blogsRouter;
